464ac2ae34e929a2a63eda1a0dea5ac9102bf53f
Spencer Boucher <spencer@spencerboucher.com>
2021-11-29 10:17:40 -0600

    Add optimizations for q1 and q2

--- q1.sql
+++ q1.sql
@@ -7,8 +7,17 @@ SET @v3 = 1828467;
 SET @v4 = 'MGT382';
 SET @v5 = 'Amber Hill';
 SET @v6 = 'MGT';
-SET @v7 = 'EE';			  
+SET @v7 = 'EE';
 SET @v8 = 'MAT';
 
 -- 1. List the name of the student with id equal to v1 (id).
--- SELECT name FROM Student WHERE id = @v1;
+
+-- The bottleneck here is the fact that there is no index on the id field of the Student table.
+-- This is discoverable by looking at the execution plan for the query and noticing that no keys are being used.
+EXPLAIN SELECT name FROM Student WHERE id = @v1;
+
+-- The solution is to create an index.
+CREATE INDEX Student_id_IDX USING BTREE ON springboardopt.Student (id);
+
+-- Then another EXPLAIN reveals that the new index is being used aind only 1 row is estimated to be read.
+EXPLAIN SELECT name FROM Student WHERE id = @v1;
--- q2.sql
+++ q2.sql
@@ -7,8 +7,25 @@ SET @v3 = 1828467;
 SET @v4 = 'MGT382';
 SET @v5 = 'Amber Hill';
 SET @v6 = 'MGT';
-SET @v7 = 'EE';			  
+SET @v7 = 'EE';
 SET @v8 = 'MAT';
 
 -- 2. List the names of students with id in the range of v2 (id) to v3 (inclusive).
--- SELECT name FROM Student WHERE id BETWEEN @v2 AND @v3;
+
+-- The potential bottleneck here is that MySQL is choosing not to use the index
+-- we previously created on Student.id, probably because the table is small
+-- enough relative to our where clause range that it is not helpful.
+
+-- This is discoverable by looking at the execution plan for the query and
+-- noticing that although the Student.id index is noted as a possible key, it is
+-- not actually being used.
+EXPLAIN SELECT name FROM Student WHERE id BETWEEN @v2 AND @v3;
+
+-- If we wanted to force use of the index, we can do so:
+SELECT name
+FROM Student
+FORCE INDEX (Student_id_IDX)
+WHERE id BETWEEN 1145072 AND 1828467;
+
+-- This does not appreciably improve the ~3ms run time of the query, however,
+-- suggesting that MySQL is correct to use a full table scan in this instance.
